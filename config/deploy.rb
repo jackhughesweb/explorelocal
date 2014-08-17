8d7f1bd62f4fb5b62d64d41dca31da3cb7d4ccc4
jackhughesweb <jack@jackhughesweb.com>
2014-08-17 22:52:37 +0100

    Edited migration

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,21 +8,47 @@ set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
-# set :pty, true
+set :pty, true
 
 set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/home/deployer/.rbenv/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
 
+  desc 'Setup config'
+  task :setup_config do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :sudo, "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_explorelocal"
+    end
+  end
+
+  desc 'Start application'
+  task :start do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      execute "#{current_path}/config/unicorn_init.sh start"
+    end
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :run, "/etc/init.d/unicorn_explorelocal stop"
+    end
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :run, "/etc/init.d/unicorn_explorelocal restart"
     end
   end
 
--- db/migrate/20140730101639_add_wikipedia_to_locations.rb
+++ db/migrate/20140730101639_add_wikipedia_to_locations.rb
@@ -3,6 +3,5 @@ class AddWikipediaToLocations < ActiveRecord::Migration
     add_column :locations, :clue_wikipedia_text, :text
     add_column :locations, :clue_wikipedia_link, :string
     remove_column :locations, :clue_wikipedia, :string
-    remove_column :locations, :clue_wikipedia_title, :string
   end
 end
