8149350cef7a7307f94aeeb314535cb0afc4f081
jackhughesweb <jack@jackhughesweb.com>
2014-08-17 17:56:26 +0100

    Added support for custom searching and improved wikitext regex

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
     var service;
     var request;
 
+    var searched = false;
+
     var flickrPage;
     var flickr_url = '';
+    var flickrSearch;
 
     var gMapsInit = false;
 
@@ -38,7 +41,8 @@ $(document).ready(function() {
         $('.location-map').attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng() + '&zoom=16&size=200x200&markers=' + results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng());
         updatePreview();
         $('.location-hidden').show();
-        $.getJSON('/locations/flickr.json', { search: results[0].name }, function(data) {
+        flickrSearch = results[0].name;
+        $.getJSON('/locations/flickr.json', { search: flickrSearch }, function(data) {
           var photos = data.flickr;
           flickrPage = data.page;
           $('.flickr_photos').html('');
@@ -73,30 +77,108 @@ $(document).ready(function() {
     }
 
     $('#getdata').on('click', function (event) {
-      $.getJSON('/locations.json', { search: $('#query_field').val() }, function(data) {
-        if (data.length > 0) {
-          var confirmation = confirm('The similar place "' + data[0].name + '" already exists. Are you sure you want to continue?');
-          if (confirmation) {
+      if (searched) {
+        var confirmation1 = confirm('New searches will reset the form. Are you sure you want to continue?');
+        if (confirmation1) {
+            $.getJSON('/locations.json', { search: $('#query_field').val() }, function(data) {
+            if (data.length > 0) {
+              var confirmation2 = confirm('The similar place "' + data[0].name + '" already exists. Are you sure you want to continue?');
+              if (confirmation2) {
+                $('.flickr_more').hide();
+                $('.location-hidden').hide();
+                if (gMapsInit) {
+                  runQuery($('#query_field').val());
+                }
+              }
+            } else {
+              $('.flickr_more').hide();
+              $('.location-hidden').hide();
+              if (gMapsInit) {
+                runQuery($('#query_field').val());
+              }
+            }
+          });
+        }
+      } else {
+        $.getJSON('/locations.json', { search: $('#query_field').val() }, function(data) {
+          if (data.length > 0) {
+            var confirmation2 = confirm('The similar place "' + data[0].name + '" already exists. Are you sure you want to continue?');
+            if (confirmation2) {
+              $('.flickr_more').hide();
+              $('.location-hidden').hide();
+              if (gMapsInit) {
+                runQuery($('#query_field').val());
+                searched = true;
+              }
+            }
+          } else {
             $('.flickr_more').hide();
             $('.location-hidden').hide();
             if (gMapsInit) {
               runQuery($('#query_field').val());
+              searched = true;
             }
           }
-        } else {
-          $('.flickr_more').hide();
-          $('.location-hidden').hide();
-          if (gMapsInit) {
-            runQuery($('#query_field').val());
-          }
+        });
+      }
+      event.preventDefault();
+    });
+
+    $('#custom_search_flickr').on('click', function (event) {
+      $('.custom_search_flickr').show();
+      $('.custom_search_flickr_info').hide();
+      event.preventDefault();
+    });
+
+    $('#getdata_flickr').on('click', function (event) {
+      flickrSearch = $('#query_flickr_field').val();
+      $.getJSON('/locations/flickr.json', { search: flickrSearch }, function(data) {
+        var photos = data.flickr;
+        flickrPage = data.page;
+        $('.flickr_photos').html('');
+        for (var i = 0; i < photos.length; i++) {
+          $('.flickr_photos').append('<img data-id="' + photos[i].id + '" src="' + photos[i].url + '" class="location-flickr-thumb">');
         }
+        $('.flickr_more').show();
+        $('.location-flickr-thumb').off('click').on('click', function () {
+          $('.location-flickr-thumb.selected').removeClass('selected');
+          $(this).addClass('selected');
+          $('#location_clue_flickr').val($(this).attr('data-id'));
+          flickr_url = $(this).attr('src');
+          updatePreview();
+        });
+      });
+      event.preventDefault();
+    });
+
+    $('#custom_search_wiki').on('click', function (event) {
+      $('.custom_search_wiki').show();
+      $('.custom_search_wiki_info').hide();
+      event.preventDefault();
+    });
+
+    $('#getdata_wiki').on('click', function (event) {
+      $.getJSON('/locations/wikisearch.json', { search: $('#query_wiki_field').val() }, function(data) {
+        var articles = data.articles;
+        $('.wiki_articles').html('');
+        for (var i = 0; i < articles.length; i++) {
+          $('.wiki_articles').append('<li><a href="#" data-id="' + articles[i].title + '" class="wiki_article">' + articles[i].title + '</a><br>' + articles[i].snippet + '</li>');
+        }
+        $('.wiki_article').off('click').on('click', function (event) {
+          $.getJSON('/locations/wikitext.json', { search: $(this).attr('data-id') }, function(data) {
+            $('#location_clue_wikipedia_link').val(data.url);
+            $('#location_clue_wikipedia_text').val(data.text);
+            updatePreview();
+          });
+          event.preventDefault();
+        });
       });
       event.preventDefault();
     });
 
     $('.flickr_more').on('click', function (event) {
       flickrPage += 1;
-      $.getJSON('/locations/flickr.json', { search: $('#location_name').val(), page: flickrPage }, function(data) {
+      $.getJSON('/locations/flickr.json', { search: flickrSearch, page: flickrPage }, function(data) {
         var photos = data.flickr;
         flickrPage = data.page;
         for (var i = 0; i < photos.length; i++) {
@@ -130,7 +212,6 @@ $(document).ready(function() {
       var wiki_text = $('#location_clue_wikipedia_text').val() || "Wiki Text";
       $('#location-preview-name').html(title);
       $('#location-preview-map').attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + lat + ',' + lng + '&zoom=16&size=200x200&markers=' + lat + ',' + lng);
-      $('#location-preview-latlng').html(lat + ', ' + lng);
       $('#location-preview-flickr-pic').attr('src', flickr_url);
       $('#location-preview-wiki-link').html(wiki_link);
       $('#location-preview-wiki-text').html(wiki_text);
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -69,7 +69,7 @@ class LocationsController < ApplicationController
       @htmltext << para.content
       @htmltext << ' '
     end
-    @text = sanitize(@htmltext, :tags=>[]).gsub(/. #{params[:search]}/, '. [The location]').gsub(/^#{params[:search]}/, '[The location]').gsub(/#{params[:search]}/, '[the location]').truncate(1000, separator: '.', omission: '.')
+    @text = sanitize(@htmltext, :tags=>[]).gsub(/[t]he #{params[:search]}/, '[the location]').gsub(/[T]he #{params[:search]}/, '[The location]').gsub(/\. #{params[:search]}/, '. [The location]').gsub(/^#{params[:search]}/, '[The location]').gsub(/#{params[:search]}/, '[the location]').truncate(1000, separator: '.', omission: '.')
   end
 
   # GET /locations/1/edit
