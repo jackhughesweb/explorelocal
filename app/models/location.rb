4d103aedd6b8c11abd7116fc123c77cb0155b57e
jackhughesweb <jack@jackhughesweb.com>
2014-10-18 20:50:37 +0100

    Added documentation for Location Model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,9 @@
+# Model for Locations
 class Location < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
+
+  # Location Search
+  # Accessed by calling Location.search
   def self.search(search)
     if search
       self.where("name like ?", "%" + search + "%")
