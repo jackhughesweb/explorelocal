fd7c0b8f7ee339aa7471362352efb96244a04a39
jackhughesweb <jack@jackhughesweb.com>
2014-10-21 12:22:51 +0100

    Fixed auth issues 2

--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe "locations" do
   it "returns a JSON object of Wikipedia articles" do
     visit "/locations/wikisearch.json?search=Worcester%20Cathedral"
-    authorize 'admin', 'admin'
+    page.driver.basic_auth('admin', 'admin')
     expect(page).to have_http_status(:success)
   end
   it "returns a JSON object of Wikipedia text" do
     visit "/locations/wikitext.json?search=Worcester%20Cathedral"
-    authorize 'admin', 'admin'
+    page.driver.basic_auth('admin', 'admin')
     expect(page).to have_http_status(:success)
   end
 end
