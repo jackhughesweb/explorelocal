035707f661a8bde49afc50afa242fc78f7c21349
jackhughesweb <jack@jackhughesweb.com>
2014-10-21 12:00:34 +0100

    Added HTTP basic auth for tests

--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe "locations" do
   it "returns a JSON object of Wikipedia articles" do
+    page.driver.browser.authorize 'admin', 'admin'
     visit "/locations/wikisearch.json?search=Worcester%20Cathedral"
     expect(page).to have_http_status(:success)
   end
   it "returns a JSON object of Wikipedia text" do
+    page.driver.browser.authorize 'admin', 'admin'
     visit "/locations/wikitext.json?search=Worcester%20Cathedral"
     expect(page).to have_http_status(:success)
   end
