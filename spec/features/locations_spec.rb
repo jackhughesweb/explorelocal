e5ef275ad43c19ea67f789cbb4385f0a50b04891
jackhughesweb <jack@jackhughesweb.com>
2014-10-24 12:48:49 +0100

    Added test for creating locations

--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "locations" do
+  it "allows a location to be created" do
+    location = Location.create!(:name => "location1", :latitude => 0.1, :longitude => 0.1, :clue_flickr => "text", :clue_wikipedia_link => "text", :clue_wikipedia_text => "text")
+    location.save
+  end
   it "returns a JSON object of Wikipedia articles" do
     page.driver.browser.authorize('admin', 'admin')
     visit "/locations/wikisearch.json?search=Worcester%20Cathedral"
